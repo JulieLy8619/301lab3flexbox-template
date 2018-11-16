ef08ba3263f3584320cbdf0f4e8bf2e53ea43708
JulieLy8619 <july8619@yahoo.com>
2018-11-15 18:34:43 -0800

    setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 301lab3flexbox-template
+
+doing lab 3 on my own in another repository to avoid merge conflicts and will update in pair prog jared lab2 if need to later
+
---
+++ js/app.js
@@ -0,0 +1,123 @@
+'use strict';
+
+// 11-15-18 jl added my stuff to this index and working from here on out
+
+//make a constructor funtion
+function Horns (obj) {
+  this.url = obj.image_url;
+  this.title = obj.title;
+  this.description = obj.description;
+  this.keyword = obj.keyword;
+  this.horn = obj.horns;
+}
+Horns.allHornsArray = [];
+Horns.listArrayKeys = [];
+Horns.listArray = [];
+Horns.filteredListArray = [];
+
+Horns.prototype.render = function() {
+  $('main').append('<div class="clone"></div>')
+  let hornClone = $('div[class="clone"]');
+  let hornHtml = $('#photo-template').html();
+  hornClone.html(hornHtml);
+  hornClone.find('h2').text(this.title);
+  hornClone.find('img').attr('src', this.url);
+  hornClone.find('p').text(this.description);
+  hornClone.removeClass('clone');
+  hornClone.attr('class', this.title);
+}
+
+Horns.prototype.makeList = function () {
+  if (!Horns.listArrayKeys.includes(this.keyword)) {
+    Horns.listArrayKeys.push(this.keyword);
+    Horns.listArray.push(this);
+  }
+}
+
+Horns.prototype.list = function () {
+  let filterList = $('select');
+  filterList.append($('<option></option>').val(this.keyword).html(this.keyword))
+};
+
+Horns.prototype.clearImage = function (){
+  $('div').remove();
+}
+
+//get information from json and populate template, which also then renders to screen.
+Horns.readJson1 = () => {
+  $.get('data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(obj => {
+        Horns.allHornsArray.push(new Horns(obj));
+      })
+    })
+    .then(Horns.loadHorns)
+    .then(Horns.populateList)
+    .then(Horns.populateForm)
+}
+
+Horns.readJson2 = () => {
+  $.get('data/page-2.json', 'json')
+    .then(data => {
+      data.forEach(obj => {
+        Horns.allHornsArray.push(new Horns(obj));
+      })
+    })
+    // console.log('while readjson2 ' + Horns.allHornsArray);
+    .then(Horns.loadHorns)
+    .then(Horns.populateList)
+    .then(Horns.populateForm)
+}
+
+Horns.loadHorns = () => {
+  Horns.allHornsArray.forEach(horn => horn.render());
+}
+
+Horns.populateForm= () => {
+  Horns.listArray.forEach( horn => horn.list());
+}
+
+Horns.populateList = () => {
+  Horns.allHornsArray.forEach(horn => horn.makeList());
+}
+Horns.clearPage = () => {
+  Horns.allHornsArray.forEach(horn => horn.clearImage());
+}
+
+$(() => Horns.readJson());
+
+
+//populates the dropdownlist
+
+//clickhandler
+
+
+$('select').on('change', function(event) {
+  //clear images
+  Horns.clearPage();
+  let getKey = event.target.value;
+  Horns.allHornsArray.forEach( hornObj => {
+    if (getKey === hornObj.keyword) {
+      // Horns.filteredListArray.push(hornObj);
+      hornObj.render();
+    }
+  })
+  // Horns.filteredListArray.forEach(horn => horn.render());
+
+});
+
+$('#page1').on('click', function(event) {
+  console.log('on page 1');
+  Horns.clearPage();
+  $(() => Horns.readJson1());
+});
+
+$('#page2').on('click', function(event) {
+  console.log('on page 2');
+  // Horns.clearPage();
+  Horns.allHornsArray = [];
+  console.log('before readjson2 ', Horns.allHornsArray);
+  $(() => Horns.readJson2());
+  console.log('after readjson2 ', Horns.allHornsArray);
+});
+
